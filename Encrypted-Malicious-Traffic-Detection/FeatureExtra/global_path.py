26121962aa097241f2b1f9d0913b1b827e78b99d
Yolandache <114452130+Yolandache@users.noreply.github.com>
2023-10-31 20:20:23 +0800

    Delete Encrypted-Malicious-Traffic-Detection directory

--- Encrypted-Malicious-Traffic-Detection/FeatureExtra/global_path.py
+++
@@ -1,5 +0,0 @@
-white_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\white'
-black_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\black'
-csv_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\csv'
-train_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\train'
-
--- Encrypted-Malicious-Traffic-Detection/train/unitTest.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("D:/Encrypted-Malicious-Traffic-Detection/total/csv/test/content.csv")[['in_packets', 'in_bytes', 'out_packets', 'out_bytes', 'duration', 'label']]
-df.index=df.index+1
-df = df.reset_index()
-df = df.rename(columns = {'ID':'id'})
-print(df)
