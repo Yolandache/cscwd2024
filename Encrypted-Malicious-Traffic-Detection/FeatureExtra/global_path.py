474cba6854dd5d1fca4c6f4d474d9755385ee74c
Yolandache <114452130+Yolandache@users.noreply.github.com>
2023-10-31 20:06:57 +0800

    Add files via upload

---
+++ Encrypted-Malicious-Traffic-Detection/FeatureExtra/global_path.py
@@ -0,0 +1,5 @@
+white_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\white'
+black_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\black'
+csv_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\csv'
+train_path = r'D:\Encrypted-Malicious-Traffic-Detection\train\data\train'
+
